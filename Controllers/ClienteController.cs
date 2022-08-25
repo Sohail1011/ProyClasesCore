50652b52d2057184933c014ce1f6e08ed6d4f801
Sohail1011 <aratavalga@gmail.com>
2022-08-24 19:02:22 -0500

    proy class

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// acceder al modelo
+using ProyClasesCore.Models;
+
+namespace ProyClasesCore.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // creamos una lista de clientes de ejemplo:
+        List<Cliente> lista = new List<Cliente>();
+
+        void PoblarClientes()
+        {
+            Cliente obj1 = new Cliente();
+            obj1.IdCliente = 1234;
+            obj1.Nombre = "Juan Perez";
+            obj1.FechaReg = DateTime.Parse("05/10/2000");
+
+            Cliente obj2 = new Cliente()
+            {
+                IdCliente = 5678, Nombre="Rosa Sanchez",
+                FechaReg = new DateTime(2001, 8, 28)
+            };
+
+            lista.Add(obj1);
+            lista.Add(obj2);
+            //
+            lista.Add(new Cliente()
+            {
+                IdCliente = 8751,
+                Nombre = "Fernando Torres",
+                FechaReg = DateTime.Today
+            });
+        }
+        public IActionResult ClienteIndex()
+        {
+            PoblarClientes();
+
+            //return View("ClienteIndex",lista);
+            return View(lista);
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyClasesCore.Models
+{
+    public class Cliente
+    {
+        // propiedades
+        // escribimos la palabra prop y
+        // presionamos 2 veces la tecla TAB
+        // IdCliente   entero
+        // Nombre      cadena
+        // FechaReg    fechatiempo
+
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaReg { get; set; }
+
+    }
+}
